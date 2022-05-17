d4cccfde45d64a7d784c2d01ffddb37322d8e350
vikkki <zhaoxiaofanjiayou@163.com>
2022-05-17 16:52:29 -0700

    Added CPU and RAM usage cards

--- pineranger.py
+++ pineranger.py
@@ -1,12 +1,22 @@
+from http.client import ImproperConnectionState
 from flask import Flask, request, render_template
 from markupsafe import escape
+from psutil import cpu_percent, virtual_memory
+import os
 
+def get_cpu_percent():
+    return cpu_percent()
+
+def get_memory_used():
+    return virtual_memory()[2]
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template("index.html")
+    data = {"cpu_percent": get_cpu_percent(),
+    "memory_used" : get_memory_used()}
+    return render_template("index.html", data=data)
 
 @app.route('/user/<username>')
 def show_user_profile(username):
